e6697f356c947150a6262d7a2c653fb7b866d1b0
Mathom10 <94836548+Mathom10@users.noreply.github.com>
2022-01-07 08:46:06 +0100

    commentaires

--- sysolaire/sysolaire/src/main/java/com/mycompany/syssolaire/Animation.java
+++ sysolaire/sysolaire/src/main/java/com/mycompany/syssolaire/Animation.java
@@ -7,11 +7,30 @@ import javafx.scene.shape.Ellipse;
 import javafx.util.Duration;
 
 public class Animation {
+    /**
+     * Chemin associé
+     */
     PathTransition pt;
+    /**
+     * Astre associé
+     */
     Astre a;
+    /**
+     * Cercle représenatant la planète
+     */
     Circle c;
+    /**
+     * Ellipse représenant l'orbite
+     */
     Ellipse e;
 
+    /**
+     * Constructeur
+     * @param pt
+     * @param a
+     * @param c
+     * @param e 
+     */
     public Animation(PathTransition pt, Astre a, Circle c, Ellipse e) {
         this.pt = pt;
         this.a = a;
@@ -19,12 +38,21 @@ public class Animation {
         this.e = e;
     }
 
+    /**
+     * Methode gérant l'affichage des animations
+     */
     public void affAnim(){
+        //Definition de la durée selon un facteur choisi
         pt.setDuration(Duration.seconds(5.77*Math.pow(10, -7)*a.getPrevolution()*3600*24));
+        //Definition de l'élément animé
         pt.setNode(c);
+        //Definition du chemin à suivre
         pt.setPath(e);
+        //Animation souhaitée infinie
         pt.setCycleCount(PathTransition.INDEFINITE);
+        //Animation à vitesse linéaire
         pt.setInterpolator(Interpolator.LINEAR);
+        //Lancement
         pt.play();
     }
 }
