495f5c71997cdbbc7f68d6181827571e793a5066
Mathom10 <94836548+Mathom10@users.noreply.github.com>
2022-01-09 13:25:48 +0100

    commentaire

--- sysolaire/sysolaire/src/main/java/com/mycompany/syssolaire/SmartGroup.java
+++ sysolaire/sysolaire/src/main/java/com/mycompany/syssolaire/SmartGroup.java
@@ -14,18 +14,34 @@ import javafx.scene.transform.Transform;
  * @author Thoma
  */
 public class SmartGroup extends Group{
+    /**
+     * Rotation
+     */
     Rotate r;
+    /**
+     * Transformation (rotation)
+     */
     Transform t = new Rotate();
 
+    /**
+     * Gestion d'une rotation autour de l'axe X
+     * @param ang 
+     */
     void rotateByX(int ang) {
         r = new Rotate(ang, Rotate.X_AXIS);
+        //Definition du point pivot (le Soleil)
         r.setPivotX(740);
         r.setPivotY(390);
+        //Rotation
         t = t.createConcatenation(r);
         this.getTransforms().clear();
         this.getTransforms().addAll(t);
     }
 
+    /**
+     * Gestion d'une rotation autour de l'axe Y
+     * @param ang 
+     */
     void rotateByY(int ang) {
         r = new Rotate(ang, Rotate.Y_AXIS);
         r.setPivotX(740);
